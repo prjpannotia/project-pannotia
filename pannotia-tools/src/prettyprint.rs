8cd2091f823095b8af2e4af93aed53417063b157
R <rqou@berkeley.edu>
2026-08-04 21:54:38 +0100

    Implement logic tile fields

--- pannotia-tools/src/prettyprint.rs
+++ pannotia-tools/src/prettyprint.rs
@@ -33,60 +33,122 @@ impl<T: Display> PrettyPrintGeneric for PrettyPrintWrap<T> {
     }
 }
 
-pub trait PrettyPrintRMUX {
-    fn pretty_print<W: std::fmt::Write>(
-        &self,
-        w: W,
-        family: Family,
-        tile_pos: TilePos,
-        tile_type: TileType,
-        i: u8,
-    ) -> std::fmt::Result;
-}
-impl PrettyPrintRMUX for &PrettyPrintWrap<mux::RMUX> {
-    fn pretty_print<W: std::fmt::Write>(
-        &self,
-        mut w: W,
-        family: Family,
-        tile_pos: TilePos,
-        tile_type: TileType,
-        i: u8,
-    ) -> std::fmt::Result {
-        if let mux::RMUX::I(rmux_inp_i) = self.0 {
-            // normal format
-            write!(w, "{}\t// ", self.0)?;
-
-            // mux purpose
-            let this_rmux = RMUX_PURPOSE[i as usize];
-            match this_rmux {
-                RMUXPurpose::SelfWire => write!(w, "rmux_self[{}]", i / 6 * 2 + i % 6 - 4)?,
-                RMUXPurpose::LeftNeighbor => write!(w, "T1_W[{}]", i / 6)?,
-                RMUXPurpose::Span4 {
-                    going_dir,
-                    wire_idx,
-                } => write!(w, "T4_{}[{}]", going_dir, wire_idx)?,
+/// Implements a more-specific pretty-print, using autoderef specialization
+macro_rules! make_specific_prettyprint {
+    ($ty:ty, $val:ident, $w:ident, $family:ident, $tile_pos:ident, $tile_type:ident, $i:ident $body:expr) => {
+        mident::mident! {
+            pub trait #concat(PrettyPrint #flatten_basename($ty)) {
+                fn pretty_print<W: std::fmt::Write>(
+                    &self,
+                    w: W,
+                    family: Family,
+                    tile_pos: TilePos,
+                    tile_type: TileType,
+                    i: u8,
+                ) -> std::fmt::Result;
             }
-
-            write!(w, " = ")?;
-
-            // mux source (decoded)
-            let rmux_src = rmux_input(i, rmux_inp_i, tile_type == TileType::BRAM);
-            match rmux_src {
-                RMUXSource::GlobalToLocal(i) => write!(w, "glb2loc[{i}]")?,
-                RMUXSource::RMUX(i) => write!(w, "rmux[{i}]")?,
-                RMUXSource::CellOutput(i) => write!(w, "this_output[{i}]")?,
-                RMUXSource::RoutingWire(src_wire) => {
-                    let abs_wire = src_wire.to_absolute(family, tile_pos);
-                    write!(
-                        w,
-                        "tile[{}] {}_{}[{}]",
-                        abs_wire.tile, abs_wire.ty, abs_wire.going_dir, abs_wire.wire_idx
-                    )?
+            impl #concat(PrettyPrint #flatten_basename($ty)) for &PrettyPrintWrap<$ty> {
+                fn pretty_print<W: std::fmt::Write>(
+                    &self,
+                    mut $w: W,
+                    $family: Family,
+                    $tile_pos: TilePos,
+                    $tile_type: TileType,
+                    $i: u8,
+                ) -> std::fmt::Result {
+                    let $val = self.0;
+                    $body
+                    Ok(())
                 }
-                _ => unreachable!(),
             }
         }
-
-        Ok(())
-    }
+    };
 }
+
+make_specific_prettyprint!(bool, val, w, _family, _tile_pos, _tile_type, _i {
+    // print bools as just "1"
+    write!(w, "{}", val as u8)?;
+});
+make_specific_prettyprint!(::bitmux::InvertedBool, val, w, _family, _tile_pos, _tile_type, _i {
+    // print bools as just "1"
+    write!(w, "{}", val.0 as u8)?;
+});
+
+make_specific_prettyprint!(u16, val, w, _family, _tile_pos, _tile_type, _i {
+    // print u16s (usually a LUT value) in hex
+    write!(w, "0x{val:04x}")?;
+});
+
+make_specific_prettyprint!(mux::RMUX, val, w, family, tile_pos, tile_type, i {
+    write!(w, "{}", val)?;
+
+    if let mux::RMUX::I(rmux_inp_i) = val {
+        // normal format
+        write!(w, "\t// ")?;
+
+        // mux purpose
+        let this_rmux = RMUX_PURPOSE[i as usize];
+        match this_rmux {
+            RMUXPurpose::SelfWire => write!(w, "rmux_self[{}]", i / 6 * 2 + i % 6 - 4)?,
+            RMUXPurpose::LeftNeighbor => write!(w, "T1_W[{}]", i / 6)?,
+            RMUXPurpose::Span4 {
+                going_dir,
+                wire_idx,
+            } => write!(w, "T4_{}[{}]", going_dir, wire_idx)?,
+        }
+
+        write!(w, " = ")?;
+
+        // mux source (decoded)
+        let rmux_src = rmux_input(i, rmux_inp_i, tile_type == TileType::BRAM);
+        match rmux_src {
+            RMUXSource::GlobalToLocal(i) => write!(w, "glb2loc[{i}]")?,
+            RMUXSource::RMUX(i) => write!(w, "rmux[{i}]")?,
+            RMUXSource::CellOutput(i) => write!(w, "this_output[{i}]")?,
+            RMUXSource::RoutingWire(src_wire) => {
+                let abs_wire = src_wire.to_absolute(family, tile_pos);
+                write!(
+                    w,
+                    "tile[{}] {}_{}[{}]",
+                    abs_wire.tile, abs_wire.ty, abs_wire.going_dir, abs_wire.wire_idx
+                )?
+            }
+            _ => unreachable!(),
+        }
+    }
+});
+
+make_specific_prettyprint!(mux::IMUX, val, w, _family, tile_pos, tile_type, i {
+    write!(w, "{}", val)?;
+
+    if let mux::IMUX::I(imux_inp_i) = val {
+        // normal format
+        write!(w, "\t// ")?;
+
+        let imux_src = if tile_type == TileType::BRAM {
+            bram_imux_input(i, imux_inp_i)
+        } else {
+            logic_imux_input(i / 4, i % 4, imux_inp_i)
+        };
+        match imux_src {
+            FunctionInputSource::RMUX(i) => {
+                write!(w, "rmux[{i}]")?
+            }
+            FunctionInputSource::LEOutput(i) => {
+                write!(w, "this_output[{i}]")?
+            }
+            FunctionInputSource::RightNeighborWire(i) => {
+                let tile_right = tile_pos + Direction::E;
+                write!(w, "tile[{}] T1_W[{}]", tile_right, i)?
+            }
+            FunctionInputSource::LeftNeighborWire(i) => {
+                let tile_right = tile_pos + Direction::W;
+                write!(w, "tile[{}] T1_E[{}]", tile_right, i)?
+            }
+            FunctionInputSource::Unused => {
+                write!(w, "vcc")?
+            }
+            _ => unreachable!(),
+        }
+    }
+});
--- pannotia-tools/src/tile_fields.rs
+++ pannotia-tools/src/tile_fields.rs
@@ -56,10 +56,43 @@ macro_rules! _dump_one_tile_field {
             writeln!($w)?;
         }
     };
+
+    // Copypasta for bool, since there's issues with Default
+    ($self:ident $w:ident @bool $fn_name:ident $fn_str:literal = $count:expr) => {
+        let count = $count;
+        for i in 0..count {
+            let setting = $self.$fn_name(i);
+            if setting {
+                write!($w, "tile[{}].{}[{}] = ", $self.pos(), $fn_str, i)?;
+                (&&PrettyPrintWrap(setting)).pretty_print(
+                    &mut $w,
+                    $self.family(),
+                    $self.pos(),
+                    $self.tile_type(),
+                    i,
+                )?;
+                writeln!($w)?;
+            }
+        }
+    };
+    ($self:ident $w:ident @bool $fn_name:ident $fn_str:literal ; ) => {
+        let setting = $self.$fn_name();
+        if setting {
+            write!($w, "tile[{}].{} = ", $self.pos(), $fn_str)?;
+            (&&PrettyPrintWrap(setting)).pretty_print(
+                &mut $w,
+                $self.family(),
+                $self.pos(),
+                $self.tile_type(),
+                0,
+            )?;
+            writeln!($w)?;
+        }
+    };
 }
 
 macro_rules! make_tile_fields {
-    ($self:ident: $($tile_ref:ident)::+ { $($func:ident $human_name:literal $eq_or_semi:tt $($count:literal)? $({ $($count_complex:tt)* })? $(;)?)* }) => {
+    ($self:ident: $($tile_ref:ident)::+ { $($(@$maybe_bool:ident)? $func:ident $human_name:literal $eq_or_semi:tt $($count:literal)? $({ $($count_complex:tt)* })? $(;)?)* }) => {
         impl<
             D: debug::DebugTracer,
             Ref: std::borrow::Borrow<Bitstream<D>>,
@@ -67,7 +100,7 @@ macro_rules! make_tile_fields {
         {
             fn dump<W: std::fmt::Write>(&$self, mut w: W) -> std::fmt::Result {
                 $({
-                    _dump_one_tile_field!{ $self w $func $human_name $eq_or_semi $($count)? $(_replace_self!{ $self { $($count_complex)* } })? }
+                    _dump_one_tile_field!{ $self w $(@$maybe_bool)? $func $human_name $eq_or_semi $($count)? $(_replace_self!{ $self { $($count_complex)* } })? }
                 })*
 
                 Ok(())
@@ -90,5 +123,21 @@ make_tile_fields! {
         async_mux "async" = 2;
         sync_load_mux "sync_load";
         sync_clr_mux "sync_clr";
+        global_to_local "glb2loc" = 4;
+        control_signal_preselect "ctrl" = 4;
+        lut "lut" = 16;
+        lut_inp_a "lut_A" = 16;
+        lut_inp_b "lut_B" = 16;
+        lut_inp_c "lut_C" = 16;
+        lut_inp_d "lut_D" = 16;
+        lc_input_c_mode "inp_c" = 16;
+        lc_carry_en "carry_en" = 16;
+        lc_clk_choice "lc_clk" = 16;
+        lc_async_choice "lc_async" = 16;
+        @bool lc_shift_reg_mode "lc_shift" = 16;
+        @bool lc_input_c_bypass_mode "lc_bypass" = 16;
+        lc_output_neigh "omux_neigh" = 16;
+        lc_output_imux "omux_imux" = 16;
+        lc_output_rmux "omux_rmux" = 16;
     }
 }
