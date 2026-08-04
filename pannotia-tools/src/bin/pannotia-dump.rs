a0b599a5f5cfbd0f71f3f3a2fcba40f6e30bfed8
R <rqou@berkeley.edu>
2026-08-04 20:25:37 +0100

    Add PNG dumping tool

--- pannotia-tools/src/bin/pannotia-dump.rs
+++ pannotia-tools/src/bin/pannotia-dump.rs
@@ -3,6 +3,7 @@
 use std::io::{self, BufWriter, Write};
 use std::process::ExitCode;
 
+use bitvec::prelude::*;
 use clap::{Parser, ValueEnum};
 
 use pannotia::prelude::*;
@@ -19,6 +20,7 @@ struct Cli {
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 enum Mode {
     Raw,
+    RawPng,
     Explain,
 }
 
@@ -67,6 +69,37 @@ fn dump_raw_bits<W: Write>(b: &Bitstream, mut wr: W) -> io::Result<()> {
     Ok(())
 }
 
+fn dump_png<W: Write>(b: &Bitstream, wr: W) -> io::Result<()> {
+    let (w, h) = b.family().main_logic_bits();
+    let mut pngenc = png::Encoder::new(wr, w, h);
+    pngenc.set_color(png::ColorType::Grayscale);
+    pngenc.set_depth(png::BitDepth::One);
+    let mut pngwr = pngenc.write_header()?;
+
+    let mut row = BitVec::<u8, Msb0>::new();
+    row.resize(w as usize, false);
+    let row_bytes = (w as usize + 7) / 8;
+
+    let mut all_img_data = Vec::new();
+    all_img_data.reserve(row_bytes * h as usize);
+
+    for y in 0..h {
+        // XXX this is a bit slow, but we can't easily avoid it because we *do* need to mirror each row
+        // (relative to the "raw" ordering slurped in from bitstream files)
+        for x in 0..w {
+            row.set(
+                x as usize,
+                b.get_logic_array_bit(debug::GlobalBitPos { y, x }),
+            );
+        }
+
+        all_img_data.extend_from_slice(&row.as_raw_slice()[..row_bytes]);
+    }
+
+    pngwr.write_image_data(&all_img_data)?;
+    Ok(())
+}
+
 fn main() -> ExitCode {
     env_logger::init();
 
@@ -83,6 +116,7 @@ fn main() -> ExitCode {
     let out = BufWriter::new(cli.output);
     let e = match cli.mode {
         Mode::Raw => dump_raw_bits(&b, out),
+        Mode::RawPng => dump_png(&b, out),
         Mode::Explain => todo!(),
     };
     match e {
